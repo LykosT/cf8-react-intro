dde6d4dda164681deae55c104ae11057508b061c
LykosT <lykourgos.top@gmail.com>
2025-09-25 19:51:28 +0300

    feat: move vite landing into a new component

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+//Second file that react read/run.
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      {/*Third file that react will read/run ---> App.tsx*/}
     <App />
   </StrictMode>,
 )
