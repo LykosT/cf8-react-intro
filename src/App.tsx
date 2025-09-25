67472325b900675b72f2a42b8897f4df23c9abfc
LykosT <lykourgos.top@gmail.com>
2025-09-25 21:03:22 +0300

    fix: alias @ for src directory

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import ClassComponent from "./components/ClassComponent";
 import FunctionalComponent from "./components/FunctionalComponent.tsx";
 import ArrowFunctionalComponent from "./components/ViteIntro/ArrowFunctionalComponent.tsx";
+import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <ClassComponent/>
           <FunctionalComponent/>
           <ArrowFunctionalComponent/>
+          <ViteIntro/>
       </>
   )
 }
