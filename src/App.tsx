534b2c29e4e67f2db493b8fdcc33eb0c82d164ec
LykosT <lykourgos.top@gmail.com>
2025-10-02 21:00:11 +0300

    feat: add NameChange using useEffect to document.title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // Third file that react will read/run.
 import Layout from "./components/Layout.tsx";
+import NameChangerWithEffect from "./components/NameChangerWithEffect.tsx";
 // import ClassComponent from "./components/ClassComponent";
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
@@ -13,10 +14,24 @@ import Layout from "./components/Layout.tsx";
 //import NameChanger from "./components/NameChanger.tsx";
 //import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
 //import CounterAdvanced from "./components/CounterAdvanced.tsx";
-import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
+//import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
 // import Button from "./components/Button.tsx";
+//import {useEffect} from "react";
+
 
 function App() {
+
+    // useEffect(setup,[]);
+    // // Fetch data from API,
+    // useEffect(() => {
+    //     //setup function
+    //     return () => {
+    //         //cleanup
+    //     }
+    //
+    // },// Dependency array
+    //   [])
+
   return (
       // <>
       //     <ClassComponent/>
@@ -26,13 +41,13 @@ function App() {
       //     <ArrowFunctionalComponentWithProps title={"Hello World!"} />
       //     <ArrowFunctionalComponentWithProps title={"Hello Coding!"} />
       //     <ArrowFunctionalComponentWithPropsType
-      //         title="Is a Arrow Functional Component With Props!"
+      //         title="Is an Arrow Functional Component With Props!"
       //         description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cumque ducimus eos."
       //     />
       //
       //     <Card title={"Card"}>
       //         <ArrowFunctionalComponentWithPropsType
-      //             title="Is a Arrow Functional Component With Props!"
+      //             title="Is an Arrow Functional Component With Props!"
       //             description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cumque ducimus eos."
       //         />
       //     </Card>
@@ -56,7 +71,8 @@ function App() {
               {/*<NameChanger/>*/}
               {/*<CounterWithMoreStates/>*/}
               {/*<CounterAdvanced/>*/}
-              <CounterWithCustomHook/>
+              {/*<CounterWithCustomHook/>*/}
+              <NameChangerWithEffect/>
 
           </Layout>
       </>
